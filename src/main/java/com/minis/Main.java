229162813ec31f0f0b4f61b70c44e6bb3fd1792c
kyclnx <158025907+kyclnx@users.noreply.github.com>
2024-08-08 11:42:35 +0800

    Update Main.java
    feets:adds

--- src/main/java/com/minis/Main.java
+++ src/main/java/com/minis/Main.java
@@ -6,7 +6,9 @@ import com.minis.test.AService;
 
 public class Main {
     public static void main(String[] args) throws NoSuchBeanDefinitionException {
+        //创建了一个对象，加载名为beans.xml的Spring配置文件
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //通过context对象从Spring容器中获取名为aservice的bean，并将其转换为AService类型。
         AService aService = (AService) context.getBean("aservice");
         aService.sayHello();
     }
